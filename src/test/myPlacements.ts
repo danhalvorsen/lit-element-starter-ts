3edd1e8dbcd84bd805976a6ace8ca44f1b3d692b
Dan Edvard Halvorsen <danhalvorsen@users.noreply.github.com>
2024-02-27 07:05:05 +0100

    MORE

--- src/test/myPlacements.ts
+++ src/test/myPlacements.ts
@@ -2,32 +2,29 @@ import {ReactiveController, ReactiveControllerHost} from 'lit';
 
 //https://lit.dev/docs/composition/controllers/#controller-host-api
 export class MyController implements ReactiveController {
-    host: ReactiveControllerHost;
+  host: ReactiveControllerHost;
 
   constructor(host: ReactiveControllerHost) {
-     this.host = host;
-     host.addController(this);
+    if (host === null) {
+      throw new Error('Host cannot be null');
+    }
+    this.host = host;
   }
-  
+
   hostConnected?(): void {
     //Draw margin
-       //Translate the coordinatesystem to left bottom
-       this.host.
-   //Draw Border
-   //Draw Padding
- }
-
-  hostDisConnected?(): void {
-    
+    //Translate the coordinatesystem to left bottom
+    //Draw Border
+    //Draw Padding
   }
-  
+
+  hostDisConnected?(): void {}
+
   hostUpdate?(): void {}
   /**
    * Called after a host update, just before the host calls firstUpdated and
    * updated. It is not called in server-side rendering.
    *
    */
-  hostUpdated?(): void {
-   
-  }
+  hostUpdated?(): void {}
 }
