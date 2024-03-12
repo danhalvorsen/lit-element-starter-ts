61809005503c3a41ee5558faff94d38fb37812ec
Dan Edvard Halvorsen <dehalvorsen@gmail.com>
2024-03-12 15:21:07 +0100

    dsdaa

---
+++ src/myRectangle.ts
@@ -0,0 +1,43 @@
+import {LitElement, html, css} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+@customElement('my-node')
+export class MyNode extends LitElement {
+  static override readonly styles = css`
+    :host {
+      display: block;
+      border: solid 1px gray;
+      padding: 16px;
+      max-width: 800px;
+    }
+  `;
+
+  override render() {
+    return html`
+      <h1>${this.sayHello(this.name)}!</h1>
+      <button @click=${this._onClick} part="button">
+        Click Count: ${this.count}
+      </button>
+      <slot></slot>
+    `;
+  }
+}
+
+export class MyElement extends MyNode {}
+
+export class GraphicalRectangle {
+  constructor(
+    public x: number,
+    public y: number,
+    public width: number,
+    public height: number
+  ) {}
+}
+
+export function GraphicalRectangle(
+  x: number,
+  y: number,
+  width: number,
+  height: number
+) {
+  return new MyRectangle(x, y, width, height);
+}
